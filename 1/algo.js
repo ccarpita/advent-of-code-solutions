23171de092a4fd04bed9f89846ce3846701fceeb
Chris Carpita <ccarpita@gmail.com>
2015-12-07 08:45:16 -0500

    Day 1

---
+++ 1/algo.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const input = fs.readFileSync('./input', 'utf-8');
+
+const op = {
+  '(': x => x + 1,
+  ')': x => x - 1
+};
+
+const basementIdx = [];
+function checkBasement(result, prev, idx) {
+  if (prev === 0 && result === -1) {
+    basementIdx.push(idx);
+  }
+  return result;
+}
+
+function algo() {
+  return input.split('')
+    .filter(c => c in op)
+    .map(c => op[c])
+    .reduce((acc, fn, idx) => checkBasement(fn(acc), acc, idx), 0)
+}
+
+if (require.main === module) {
+  console.log('final floor: ', algo());
+  console.log('first basement char: ', basementIdx[0] + 1);
+}
+module.exports = algo;
